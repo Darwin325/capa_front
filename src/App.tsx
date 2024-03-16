385b8675c212e19da4a0c103e1fb78b1003b336d
Darwin Zamora <darwinzc325@gmail.com>
2024-03-16 18:20:14 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import { Series } from "@/pages/Series"
+
+function App() {
+  return (
+    <>
+      <Series />
+      <h1 className="text-red-500">Hello world jaj!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Series.tsx
@@ -0,0 +1,7 @@
+export function Series() {
+  return (
+    <div className="border border-red-500">
+      <h1 className="text-red-500">Series</h1>
+    </div>
+  )
+}
